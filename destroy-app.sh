68cb7eea8ba11f1b1a3cfd3f84e83279b925d0a0
Luis Meneses <luismenesesep@gmail.com>
2024-01-13 13:11:48 -0500

    app name added

--- destroy-app.sh
+++ destroy-app.sh
@@ -6,6 +6,8 @@ NC='\033[0m'
 
 echo -e "${YELLOW}Destroying Ephemeral Environment${NC}"
 
+APP_NAME="${GITHUB_REPOSITORY_NAME}-pr-${GITHUB_PR_NUMBER}"
+
 heroku apps:destroy --app $APP_NAME --confirm $APP_NAME
 
 echo -e "${GREEN}Completed.${NC}"
