d4e7fb6996b460c764fcd605e405df15dfb34f1c
Luis Meneses <luismenesesep@gmail.com>
2024-01-13 01:04:48 -0500

    revert

--- destroy-app.sh
+++ destroy-app.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-REPO_NAME_AFTER_FIRST_DASH=$(echo "$GITHUB_REPOSITORY_NAME" | cut -d '-' -f2-)
 
-APP_NAME="${REPO_NAME_AFTER_FIRST_DASH}-pr-${GITHUB_PR_NUMBER}"
+APP_NAME="${GITHUB_REPOSITORY_NAME//\//-}-pr-${GITHUB_PR_NUMBER}"
 
 heroku apps:destroy --app $APP_NAME --confirm $APP_NAME
