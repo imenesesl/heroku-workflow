50c19f9279ce029ba2808e3dd2a90bf9b577fbe3
Luis Meneses <luismenesesep@gmail.com>
2024-01-13 01:43:42 -0500

    labels added

--- create-app.sh
+++ create-app.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 APP_NAME="${GITHUB_REPOSITORY_NAME//\//-}-pr-${GITHUB_PR_NUMBER}"
 
+echo -e "${YELLOW}Creation Ephemeral Environment${NC}"
+echo -e "${YELLOW}Verifying the existence of: ${APP_NAME}${NC}"
+
 if heroku apps:info $APP_NAME &> /dev/null; then
-    echo "environment $APP_NAME created."
+    echo -e "${GREEN}Ephemeral Environment ${APP_NAME} created.${NC}"
 else
+    echo -e "${YELLOW}Creating new Ephemeral Environment: ${APP_NAME}${NC}"
     heroku create $APP_NAME
 fi
+
+echo -e "${GREEN}Completed.${NC}"
