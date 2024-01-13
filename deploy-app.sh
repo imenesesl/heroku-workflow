c8cd5bdf4f678d4105427de1257cecc44ee318b3
Luis Meneses <luismenesesep@gmail.com>
2024-01-13 02:24:27 -0500

    app name fixed

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 APP_NAME="${GITHUB_REPOSITORY_NAME//\//-}-pr-${GITHUB_PR_NUMBER}"
+
+echo -e "${YELLOW}Updating Ephemeral Environment: ${APP_NAME} ${NC}"
 
 heroku auth:token
 heroku git:remote -a $APP_NAME
