3c2569b1964124bcb98ad853ab1e653d5e5734bd
Luis Meneses <luismenesesep@gmail.com>
2024-01-15 20:42:45 -0500

    deploy script added

--- deploy-app.sh
+++ deploy-app.sh
@@ -3,7 +3,7 @@ heroku login --api-key $HEROKU_API_KEY
 git remote add heroku https://git.heroku.com/$APP_NAME.git
 git push heroku HEAD:main --force
 
-APP_URL=$(heroku apps:info -a ${{ inputs.app_name }} | grep 'Web URL' | awk '{print $NF}')
+APP_URL=$(heroku apps:info -a $APP_NAME | grep 'Web URL' | awk '{print $NF}')
 
 echo "----------------------------------------"
 echo "🚀 Deployment URL is $APP_URL 🚀"
